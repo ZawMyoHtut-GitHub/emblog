69ac6a99ad5ced4c99fcad305134062306c6a3b3
Your Name <you@example.com>
2021-07-07 18:37:16 +0630

    second commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -82,7 +82,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Zaw Myo Htut
                 </div>
 
                 <div class="links">
